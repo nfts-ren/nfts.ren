548ffe6b4e55febe859ee69909ab7545b7e65e1f
nfts-ren <accounts@nfts.ren>
2021-06-01 09:28:41 +1000

    Updates

--- generateMetadata.js
+++ generateMetadata.js
@@ -1,27 +1,28 @@
-const fs = require('fs')
-const util = require('util')
-const path = require('path')
-const BN = require('bn.js')
-const rimraf = require('rimraf')
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+const BN = require("bn.js");
+const rimraf = require("rimraf");
 
-const writeFile = util.promisify(fs.writeFile)
-const mkdir = util.promisify(fs.mkdir)
-const rmdir = util.promisify(rimraf)
+const writeFile = util.promisify(fs.writeFile);
+const mkdir = util.promisify(fs.mkdir);
+const rmdir = util.promisify(rimraf);
 
-const EpochData1 = require('../src/hooks/merkle/1.json')
-const EpochData2 = require('../src/hooks/merkle/2.json')
-const EpochData3 = require('../src/hooks/merkle/3.json')
-const EpochData4 = require('../src/hooks/merkle/4.json')
-const EpochData5 = require('../src/hooks/merkle/5.json')
-const EpochData6 = require('../src/hooks/merkle/6.json')
-const EpochData7 = require('../src/hooks/merkle/7.json')
-const EpochData8 = require('../src/hooks/merkle/8.json')
-const EpochData9 = require('../src/hooks/merkle/9.json')
-const EpochData10 = require('../src/hooks/merkle/10.json')
-const EpochData11 = require('../src/hooks/merkle/11.json')
-const EpochData12 = require('../src/hooks/merkle/12.json')
-const EpochData13 = require('../src/hooks/merkle/13.json')
-const EpochData14 = require('../src/hooks/merkle/14.json')
+const EpochData1 = require("../src/hooks/merkle/1.json");
+const EpochData2 = require("../src/hooks/merkle/2.json");
+const EpochData3 = require("../src/hooks/merkle/3.json");
+const EpochData4 = require("../src/hooks/merkle/4.json");
+const EpochData5 = require("../src/hooks/merkle/5.json");
+const EpochData6 = require("../src/hooks/merkle/6.json");
+const EpochData7 = require("../src/hooks/merkle/7.json");
+const EpochData8 = require("../src/hooks/merkle/8.json");
+const EpochData9 = require("../src/hooks/merkle/9.json");
+const EpochData10 = require("../src/hooks/merkle/10.json");
+const EpochData11 = require("../src/hooks/merkle/11.json");
+const EpochData12 = require("../src/hooks/merkle/12.json");
+const EpochData13 = require("../src/hooks/merkle/13.json");
+const EpochData14 = require("../src/hooks/merkle/14.json");
+const EpochData15 = require("../src/hooks/merkle/15.json");
 
 const epochs = [
   EpochData1,
@@ -38,7 +39,8 @@ const epochs = [
   EpochData12,
   EpochData13,
   EpochData14,
-]
+  EpochData15,
+];
 
 const template = (name, epoch, mediaFile, artist) => {
   const metadata = {
@@ -55,37 +57,37 @@ const template = (name, epoch, mediaFile, artist) => {
       },
     ],
     image: mediaFile.replace(
-      '/images/',
-      'ipfs://QmcpYkFFpA3x54ZbmxWGoocZjhWsBsEbp8UuYAcy9tgxWB/'
+      "/images/",
+      "ipfs://QmS9FXpRiMtDuHGxrrbzJtUFGzXNs5mtsRHT74hX6DvShU/"
     ),
-  }
+  };
   if (artist) {
-    metadata.artist = artist
+    metadata.artist = artist;
   }
-  return metadata
-}
+  return metadata;
+};
 
-const tokenIdSeparator = 100000000
+const tokenIdSeparator = 100000000;
 
 const main = async () => {
-  await rmdir('./metadata')
-  await mkdir('./metadata')
+  await rmdir("./metadata");
+  await mkdir("./metadata");
 
   for (const epoch of epochs) {
     for (let i = 0; i < epoch.nfts.length; i++) {
-      const nft = epoch.nfts[i]
-      const metadata = template(nft.name, epoch.id, nft.url, epoch.artist)
-      const hex = tokenIdSeparator * epoch.id + i + 1
+      const nft = epoch.nfts[i];
+      const metadata = template(nft.name, epoch.id, nft.url, epoch.artist);
+      const hex = tokenIdSeparator * epoch.id + i + 1;
       const hexBuffer = new BN(hex)
-        .toArrayLike(Buffer, 'be', 32)
-        .toString('hex')
-      await mkdir(`./metadata/${hexBuffer}`)
+        .toArrayLike(Buffer, "be", 32)
+        .toString("hex");
+      await mkdir(`./metadata/${hexBuffer}`);
       await writeFile(
         `./metadata/${hexBuffer}/metadata.json`,
-        JSON.stringify(metadata, null, '  ')
-      )
+        JSON.stringify(metadata, null, "  ")
+      );
     }
   }
-}
+};
 
-main().catch(console.error)
+main().catch(console.error);
